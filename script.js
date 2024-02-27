a8a621dd8dfb9ddb9a4c71d375df3ec883b26104
ShreyaKumar-dev <shreyatiwary2003@gmail.com>
2024-02-28 00:38:27 +0530

    javascript mess

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const lapButton = document.getElementById("lap")[0];
 const pauseButton = document.getElementById("pause")[0];
 const playButton = document.getElementById("play")[0];
 const resetButton = document.getElementById("reset")[0];
+const second = document.getElementById("sec")[0];
 
 let isPlay = false
+let secCounter = 0;
+let sec;
 
 const toggleButton = () => {
     lapButton.classList.remove("hidden")
@@ -13,17 +16,23 @@ const toggleButton = () => {
 const play = () => {
     if (!isPlay) {
         playButton.innerHTML = 'Pause';
+        sec  = setInterval(() => {
+                second.innerHTML = secCounter++;
+            }, 1000);
         isPlay = true;
     } else{
         playButton.innerHTML = 'Play';
+        clearInterval(sec);
         isPlay = false;
     }
     toggleButton();
 }
 
 const reset = () => {
+    play();
     lapButton.classList.add("hidden")
-    resetButton.classList.add("hidden")
+    resetButton.classList.add("hidden")  
 }
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
